c79c26314ab95121b5f9831800574d8b89937a21
kuweidashi <89897887+kuweidashi@users.noreply.github.com>
2022-10-27 23:31:36 +0800

    Create Class19.java

---
+++ Class19.java
@@ -0,0 +1,24 @@
+package Java10;
+
+class CRectangle
+{
+   protected int length;
+   protected int width;
+   public CRectangle(int l, int w)
+   {
+      length=l;
+      width=w;
+   }
+   public String toString()
+   {
+      return "length="+length+","+"width="+width+","+"area="+length*width;
+   }
+}
+
+public class Class19
+{
+   public static void main(String args[])
+   {
+      CRectangle rect = new CRectangle(2, 6);
+      System.out.println(rect);
+   }
