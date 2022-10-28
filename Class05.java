630443d03e9496ee2a3b033574b74ace27a7c8c8
kuweidashi <89897887+kuweidashi@users.noreply.github.com>
2022-10-28 08:54:10 +0800

    Create Class05.java

---
+++ Class05.java
@@ -0,0 +1,34 @@
+package Java10;
+
+class Caaa
+{
+   private int num;
+
+   public Caaa(int n)
+   {
+      num=n;
+   }
+   public int get()
+   {
+      return num;
+   }
+}
+class Cbbb extends Caaa
+{
+   public Cbbb(int n)
+   {
+      super(n);
+   }
+   public void show()
+   {
+      System.out.println("num="+get());
+   }
+}
+public class Class05
+{
+   public static void main(String args[])
+   {
+      Cbbb bb = new Cbbb(2);
+      bb.show();
+   }
+}
